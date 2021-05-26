f0236c02fa50c92d25c4d294a5464843194a105e
jlpeppers1 <Monkey#12fuckgithubshit>
2021-05-25 21:38:54 -0500

    Added Topic 3 python file

---
+++ main/cast_to_integer.py
@@ -0,0 +1,20 @@
+"""
+Program: cast_to_integer.py
+Author: Joe Peppers
+Last date modified: 05/25/2021
+
+The purpose of this program is to accept any input, 
+convert to a integer type and output the integer. 
+"""
+#Write a small program that accepts any input and converts it to an integer, include docsting at the beginning of your code.
+#Test your code by observing the results and adding comments at the bottom of your .py file
+
+
+
+
+
+# Input   Expected Output  Actual Output
+#    5               5                       5
+#   6.7             6                       6
+#   'somestr'  ?                       ?
+#Use different data than presented above. What happens with the string input?
